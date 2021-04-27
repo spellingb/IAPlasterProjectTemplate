0334726e1210a6d81a405b58e9757c7a97436467
spellingb <20215364+spellingb@users.noreply.github.com>
2021-04-27 15:22:00 -0500

    Updated build tasks

--- Build/Module.B.local.ps1
+++ Build/Module.B.local.ps1
@@ -1,6 +1,51 @@
+#Requires -Module @{ ModuleName = 'InvokeBuild'; ModuleVersion = 5.7 }
+
+[CmdletBinding()]
+[System.Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidUsingEmptyCatchBlock', '')]
+[System.Diagnostics.CodeAnalysis.SuppressMessage('PSPossibleIncorrectComparisonWithNull', '')]
+
+param (
+	[string[]]$Tag,
+	[ValidateSet('Major','Minor','Patch')]
+	$DeploymentType = 'Patch'
+)
+
 # Ensure everything works in the most strict mode.
 Set-StrictMode -Version Latest
 
+#region Get OS
+try {
+	if ( $thething -eq $null){}
+    $script:IsWindows = (-not (Get-Variable -Name IsWindows -ErrorAction Ignore)) -or $IsWindows
+    $script:IsLinux = (Get-Variable -Name IsLinux -ErrorAction Ignore) -and $IsLinux
+    $script:IsMacOS = (Get-Variable -Name IsMacOS -ErrorAction Ignore) -and $IsMacOS
+    $script:IsCoreCLR = $PSVersionTable.ContainsKey('PSEdition') -and $PSVersionTable.PSEdition -eq 'Core'
+}
+catch { }
+
+switch ($true) {
+    { $IsWindows } {
+        $OS = "Windows"
+        if (-not ($IsCoreCLR)) {
+            $OSVersion = $PSVersionTable.BuildVersion.ToString()
+        }
+    }
+    { $IsLinux } {
+        $OS = "Linux"
+    }
+    { $IsMacOs } {
+        $OS = "OSX"
+    }
+    { $IsCoreCLR } {
+        $OSVersion = $PSVersionTable.OS
+    }
+}
+#endregion
+
+#Ensure all Build Tools exist
+Import-Module "$PSScriptRoot/Tools/BuildTools.psm1" -Force -ErrorAction Stop
+
+$publishRepository = 'ArmorPosh'
 $RepoRoot = $PSScriptRoot
 $ModuleName = '<%=$PLASTER_PARAM_ModuleName%>'
 $ModulePath = Join-Path $RepoRoot $ModuleName
@@ -10,17 +55,30 @@ $DocsLocale = 'en-US'
 $ModuleManifestPath = Join-Path $ModulePath "$ModuleName.psd1"
 $LocalBuildPath = Join-Path $RepoRoot 'build'
 
-Add-BuildTask GetVersion {
+Add-BuildTask Init { Invoke-Init }
+
+# Synopsis: Get the next Version for the build
+Add-BuildTask GetVersion Init, {
 	if (Test-Path $ModuleManifestPath) {
-		$manifest = Test-ModuleManifest -Path $ModuleManifestPath
-		[System.Version]$version = $manifest.Version
-		# Write-Host "Old Version: $version"
-		$env:version = New-Object -TypeName System.Version -ArgumentList ($version.Major, $version.Minor, ($version.Build + 1 ))
-		# Write-Host "New Version: $env:version"
+		$manifestVersion = [Version](Get-Metadata -Path $env:BHPSModuleManifest)
+
+		$CurrentOnlineVersion = [Version](Find-Module -Name $env:BHProjectName).Version
+
+		$env:CurrentOnlineVersion = [version]$CurrentOnlineVersion
+
+		if ( 	( $manifestVersion.Major -gt $CurrentOnlineVersion.Major ) -or
+				( $manifestVersion.Minor -gt $CurrentOnlineVersion.Minor ) -or
+				( $manifestVersion.Build -gt $CurrentOnlineVersion.Build )
+			) {
+				$env:NextBuildVersion = [Version]( Step-Version $manifestVersion -By $DeploymentType)
+
+			} else {
+			$env:NextBuildVersion = [Version]( Step-Version $env:CurrentOnlineVersion -By $DeploymentType)
+		}
+
 		Try {
-			# Update the manifest with the new version value and fix the weird string replace bug
-			$functionList = ((Get-ChildItem -Path $PublicFunctionsPath -Recurse -Filter "*.ps1").BaseName)
-			Update-ModuleManifest -Path $ModuleManifestPath -ModuleVersion $env:version -FunctionsToExport $functionList
+			$functionList = ( ( Get-ChildItem -Path $env:BHModulePath\Public -Recurse -Filter "*.ps1").BaseName )
+			Update-ModuleManifest -Path $env:BHPSModuleManifest -ModuleVersion $env:NextBuildVersion -FunctionsToExport $functionList
 			(Get-Content -Path $ModuleManifestPath) -replace "PSGet_$ModuleName", "$ModuleName" | Set-Content -Path $ModuleManifestPath
 			(Get-Content -Path $ModuleManifestPath) -replace 'NewManifest', "$ModuleName" | Set-Content -Path $ModuleManifestPath
 			(Get-Content -Path $ModuleManifestPath) -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' | Set-Content -Path $ModuleManifestPath -Force
@@ -33,9 +91,10 @@ Add-BuildTask GetVersion {
 	}
 }
 
+#Synopsis: Run pester Tests
 Add-BuildTask PesterTests {
 	try {
-		$testResultsFile = "$RepoRoot\TestResult.xml"
+		$testResultsFile = "$env:BHProjectPath\TestResult.xml"
 		$result = Invoke-Pester -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
 		Remove-Item $testResultsFile -Force
 		Assert-Build ($result.FailedCount -eq 0) "$($result.FailedCount) Pester test(s) failed."
@@ -44,7 +103,8 @@ Add-BuildTask PesterTests {
 	}
 }
 
-Add-BuildTask UpdateDocs {
+#Synopsis: Make Help Files
+Add-BuildTask UpdateDocs  {
 	# Check docs folder
 	if (-not (Test-Path $DocsPath)){
 		try{
@@ -54,34 +114,79 @@ Add-BuildTask UpdateDocs {
 		}
 	}
 	# Check project structure
-	if (-not ((Test-Path $DocsPath) -and (Test-Path $ModulePath))) {
+	if (-not ((Test-Path $DocsPath) -and (Test-Path $env:BHModulePath))) {
 		throw "Repository structure does not look OK"
 	} else {
 		if (Get-Module -ListAvailable -Name platyPS) {
 			# Import modules
 			Import-Module platyPS
-			Import-Module $modulePath -Force
+			Import-Module $env:BHModulePath -Force
 
 			# Generate markdown for new cmdlets
 			New-MarkdownHelp -Module $ModuleName -OutputFolder $docsPath -Locale $DocsLocale -UseFullTypeName -ErrorAction SilentlyContinue | Out-Null
 			# Update markdown for existing cmdlets
 			Update-MarkdownHelp -Path $docsPath -UseFullTypeName | Out-Null
 			# Generate external help
-			New-ExternalHelp -Path $docsPath -OutputPath (Join-Path -Path $modulePath -ChildPath $DocsLocale) -Force | Out-Null
+			New-ExternalHelp -Path $docsPath -OutputPath (Join-Path -Path $BHModulePath -ChildPath $DocsLocale) -Force | Out-Null
 		} else {
 			throw "You require the platyPS module to generate new documentation"
 		}
 	}
 }
 
-Add-BuildTask LocalBuild GetVersion, PesterTests, UpdateDocs, {
-    $BuildFolderName = ('{0}\{1}' -f $ModuleName, (Test-ModuleManifest -Path $ModuleManifestPath).Version)
-    $BuildFolderPath = (Join-Path $LocalBuildPath $BuildFolderName)
-    if(Test-Path $BuildFolderPath) {
-        Remove-Item $BuildFolderPath -Force -Recurse
-    }
-	$BuildFolder = New-Item -Path $LocalBuildPath -Name $BuildFolderName -ItemType Directory
-	Copy-Item -Path (Join-Path $RepoRoot "$ModuleName\*") -Destination $BuildFolder -Recurse
+#Synopsis: Show Debugging information
+Add-Buildtask ShowInfo {
+    Write-Build Gray
+    Write-Build Gray ('Running in:                 {0}' -f $env:BHBuildSystem)
+    Write-Build Gray '-------------------------------------------------------'
+    Write-Build Gray
+    Write-Build Gray ('Project name:               {0}' -f $env:BHProjectName)
+    Write-Build Gray ('Project root:               {0}' -f $env:BHProjectPath)
+    Write-Build Gray ('Build Path:                 {0}' -f $env:BHBuildOutput)
+    Write-Build Gray ('Current (online) Version:   {0}' -f $env:CurrentOnlineVersion)
+    Write-Build Gray '-------------------------------------------------------'
+    Write-Build Gray
+    Write-Build Gray ('Branch:                     {0}' -f $env:BHBranchName)
+    Write-Build Gray ('Commit:                     {0}' -f $env:BHCommitMessage)
+    Write-Build Gray ('Build #:                    {0}' -f $env:BHBuildNumber)
+    Write-Build Gray ('Next Version:               {0}' -f $env:NextBuildVersion)
+    Write-Build Gray '-------------------------------------------------------'
+    Write-Build Gray
+    Write-Build Gray ('PowerShell version:         {0}' -f $PSVersionTable.PSVersion.ToString())
+    Write-Build Gray ('OS:                         {0}' -f $OS)
+    Write-Build Gray ('OS Version:                 {0}' -f $OSVersion)
+    Write-Build Gray
 }
 
-Add-BuildTask . LocalBuild
+Add-BuildTask LocalBuild GetVersion, PesterTests, UpdateDocs, {
+	#Create New Build folder
+	$null = New-Item -Path "$env:BHBuildOutput\$env:BHProjectName\$env:NextBuildVersion" -ItemType Directory
+
+	#copy Module to new Folder
+	Copy-Item -Path "$env:BHModulePath/*" -Destination "$env:BHBuildOutput\$env:BHProjectName\$env:NextBuildVersion" -Recurse -Force
+
+}
+
+#Synopsis: Publish Build on Github and configured REpo
+Add-BuildTask Publish Scrub, {
+	Publish-Module -Path "$env:BHBuildOutput\$env:BHProjectName\$env:NextBuildVersion" -Repository $PublishRepository
+
+	$releaseText = "Release version $env:NextBuildVersion"
+
+    Write-Build Gray "git checkout $ENV:BHBranchName"
+    cmd /c "git checkout $ENV:BHBranchName 2>&1"
+
+    Write-Build Gray "git tag -a v$env:NextBuildVersion -m `"$releaseText`""
+	cmd /c "git tag -a v$env:NextBuildVersion -m `"$releaseText`" 2>&1"
+
+	Write-Build Gray "git commit -a -m '$releaseText'"
+	cmd /c "git commit -a -m '$releaseText' --verbose"
+
+	Write-Build gray "git push --verbose"
+	cmd /c "git push --verbose 2>&1"
+
+    Write-Build Gray "git push origin v$env:NextBuildVersion"
+    cmd /c "git push origin v$env:NextBuildVersion 2>&1"
+}
+
+Add-BuildTask . ShowInfo, LocalBuild, Publish
